0ad46910ecab3579aec17d2b6ada291addae751d
Kevin Glynn <kevin.glynn@gmail.com>
2024-03-07 18:11:48 +0100

    Day 1 - Part 1

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,21 @@
 module Main where
 
+import Data.Char (isDigit)
+
+toCV :: String -> Int
+toCV w
+  | null ld = 0
+  | otherwise  = read $ head ld:[head rd]
+  where
+    ld = dropWhile (not . isDigit) w
+    rd = dropWhile (not . isDigit) $ reverse w
+
+
+day1 :: IO ()
+day1 = do
+       inp <- getContents
+       let cv = sum $ map toCV $ words inp
+       print cv
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = day1
