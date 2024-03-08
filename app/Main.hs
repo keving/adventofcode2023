2b73f6f280797c6a0d7b93346415e0c6f0cf291b
Kevin Glynn <kevin.glynn@gmail.com>
2024-03-08 13:18:49 +0100

    Allow written words for digits

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,28 @@
 module Main where
 
-import Data.Char (isDigit)
+import Data.List (isPrefixOf, tails)
+import Data.Maybe (mapMaybe)
+
+numbers :: [(String, Int)]
+numbers = [(show n, n) | n <- [0..9]] ++
+  [("zero", 0),
+   ("one", 1),
+   ("two", 2),
+   ("three", 3),
+   ("four", 4),
+   ("five", 5),
+   ("six", 6),
+   ("seven", 7),
+   ("eight", 8),
+   ("nine", 9)]
 
 toCV :: String -> Int
 toCV w
-  | null ld = 0
-  | otherwise  = read $ head ld:[head rd]
+  | null digits = 0
+  | otherwise   = head digits * 10 + last digits
   where
-    ld = dropWhile (not . isDigit) w
-    rd = dropWhile (not . isDigit) $ reverse w
-
+    -- Find digits for every substring in w.
+    digits = concatMap (\sub -> mapMaybe (\(p,d) -> if p `isPrefixOf` sub then Just d else Nothing) numbers) $ tails w
 
 day1 :: IO ()
 day1 = do
